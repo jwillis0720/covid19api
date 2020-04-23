5d3cc5102ba701326b8ebf367387832b8e89e003
Jordan Willis <jordanwillis@Jordans-MacBook-Pro.local>
2020-04-22 20:08:31 -0500

    added state

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -2,39 +2,7 @@ const { ApolloServer, gql } = require('apollo-server');
 const fetch = require("node-fetch");
 const { RESTDataSource } = require('apollo-datasource-rest');
 const { resolveAs } = require('graphql-directive-resolve-as');
-
-
-const typeDefs = `
-   directive @resolveAs(name: String) on FIELD_DEFINITION
-   type Query {
-    Countries(id:Int): [Country]!
-  }
-  type CountryInfo{
-      id: Int @resolveAs(name: "_id")
-      iso2: String
-      iso3: String
-      lat: Float
-      long: Float
-      flag: String
-  }
-  type Country {
-      name: String @resolveAs(name: "country")
-      updated: Float,
-      info: CountryInfo @resolveAs(name: "countryInfo") 
-      cummulativeCases: Int @resolveAs(name: "cases")
-      todayCases: Int,
-      cummulativeDeaths: Int @resolveAs(name: "deaths")
-      todayDeaths: Int,
-      recovered: Int,
-      active: Int,
-      critical: Int,
-      casesPerOneMillion: Int,
-      deathsPerOneMillion: Int,
-      tests: Int,
-      testsPerOneMillion: Int,
-      continent: String
-  }
-`;
+const typeDefs = require('./schema')
 
 
 class NovelCovidAPI extends RESTDataSource {
@@ -43,21 +11,59 @@ class NovelCovidAPI extends RESTDataSource {
         this.baseURL = 'https://corona.lmao.ninja/v2/';
     }
 
-    async getCountries(id) {
-        if (id != null) {
-            const data = []
-            data.push(this.get(`countries/${id}`))
-            return data
-        } else {
-            return this.get("countries")
-        }
+    async getCountries() {
+        return this.get("countries")
     }
+    async getStates() {
+        return this.get("states/")
+    }
+
+    async getStatebyName(name) {
+        const res = await this.get(`states/${name}`)
+        // console.log(res)
+        return res
+    }
+
+
+    async getCountrybyID(id) {
+        return this.get(`countries/${id}`)
+
+    }
+
+    async getTimeLinebyState(name) {
+        console.log(name)
+        const res = await this.get(`nyt/states/${name}`)
+        return res
+    }
+    // if (id != null) {
+    //     const data = []
+    //     data.push(this.get(`countries/${id}`))
+    //     return data
+    // } else {
+
+    //     // }
+    // }
 }
 
 const resolvers = {
     Query: {
-        Countries: async (_, { id }, { dataSources }) => {
-            return dataSources.ncapi.getCountries(id)
+        Countries: async (_parent, _args, { dataSources }) => {
+            return dataSources.ncapi.getCountries()
+        },
+        Country: async (_, { id }, { dataSources }) => {
+            return dataSources.ncapi.getCountrybyID(id)
+        },
+        States: async (_parent, _args, { dataSources }) => {
+            return dataSources.ncapi.getStates()
+        },
+        State: async (_parent, { name }, { dataSources }) => {
+            // console.log(name)
+            return dataSources.ncapi.getStatebyName(name)
+        }
+    },
+    State: {
+        timeline: async (state, _, { dataSources }) => {
+            return dataSources.ncapi.getTimeLinebyState(state.state)
         }
     }
 
